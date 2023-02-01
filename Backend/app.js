ffeea8dd3d253e5713ba99de6c5084899247e8f7
OumarYanni <oumaryanni@gmail.com>
2023-02-01 17:50:23 +0000

    Création du model "users"

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,10 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+//Importation du modèle "user"
+const userThing = require('./models/user');
 
+//Connexion à MongoDB
 mongoose.connect('mongodb+srv://OumarYanni:RN0mDjLHEEWYLpg0@cluster0-piiquante.5cgwwky.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -20,33 +23,24 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
+// Route Post : Ici, on crée une instance du modèle userThing en lui passant un objet JavaScript contenant toutes les informations requises du corps de requête analysé (en ayant supprimé en amont le faux_id envoyé par le front-end).
+app.post('/api/auth/signup', (req, res, next) => {
+  delete req.body._id;
+  const userThing = new userThing({
+    ...req.body
   });
+  //Ce modèle comporte une méthode save() qui enregistre simplement le Thing dans la base de données.
+  //La méthode save() renvoie une Promise. Ainsi, dans notre bloc then() , nous renverrons une réponse de réussite avec un code 201 de réussite. Dans notre bloc catch() , nous renverrons une réponse avec l'erreur générée par Mongoose ainsi qu'un code d'erreur 400.
+  userThing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
+//récupération de la liste de "things" en vente
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));
   });
 
 module.exports = app;
