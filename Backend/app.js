03711b8ade90ff88681330db633746737144fd40
OumarYanni <oumaryanni@gmail.com>
2023-02-22 18:03:25 +0000

    Intégration de password validator et finalisation

--- Backend/app.js
+++ Backend/app.js
@@ -1,48 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Sécurité
 //On importe helmet" qui est un module Node.js qui permet de renforcer la sécurité de l'application en définissant différents en-têtes HTTP pour les réponses HTTP.
-const helmet = require('helmet');
+const helmet = require("helmet");
 
 //Importation de la librairie dotenv et de configurer les variables d'environnement définies dans un fichier .env. Les variables d'environnement sont des informations sensibles telles que les mots de passe, les clés API, etc. qui ne doivent pas être partagées publiquement. La librairie dotenv les charge dans l'environnement Node.js et peut les utiliser dans le code en utilisant process.env.
 const dotenv = require("dotenv").config();
 
-//La constante passwordValidator est une instance du package express-validator qui est un middleware pour la validation de données dans Express.js. Il vérifie si les données envoyées dans une requête HTTP sont valides selon les règles de validation spécifiées. Par exemple, si une validation de mots de passe est requise, passwordValidator pourrait être utilisé pour vérifier si le mot de passe respecte les critères tels que la longueur minimale, la présence de caractères spéciaux, etc. Cela aide à garantir la sécurité des données en s'assurant que les entrées utilisateur sont valides avant de les traiter sur le serveur.
-const passwordValidator = require('password-validator');
-
-
-
 //Importation du router user
-const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce');
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
 
 //importation dans app.js pour accéder au path de notre serveur
-const path = require('path');
+const path = require("path");
 
 //Importation du modèle "user"
 // const userThing = require('./models/user');
 
 //Connexion à MongoDB
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 
 // Cela indique à Express qu'il faut gérer la ressource images de manière statique (un sous-répertoire de notre répertoire de base, __dirname) à chaque fois qu'elle reçoit une requête vers la route /images.
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Ajout de la sécurité contre les attaques XSS
 app.use(helmet());
@@ -52,34 +54,9 @@ app.use(helmet());
 //     })
 // );
 
-//Création d'un schéma pour la validation du mot de passe
-const schema = new passwordValidator();
-
-//Définition des règles pour la validation du mot de passe
-schema
-  .is().min(8)                                    // Longueur minimale du mot de passe
-  .is().max(100)                                  // Longueur maximale du mot de passe
-  .has().uppercase()                              // Au moins une majuscule
-  .has().lowercase()                              // Au moins une minuscule
-  .has().digits()                                 // Au moins un chiffre
-  .has().symbols()                                // Au moins un symbole
-  .has().not().spaces();                         // Pas d'espace
-
-  //Middleware pour la validation du mot de passe
-const validatePassword = (req, res, next) => {
-    const { password } = req.body;
-    
-    if (!schema.validate(password)) {
-      return res.status(400).json({ error: 'Mot de passe non valide' });
-    }
-    next();
-  };
-app.use('/api/auth/signup', validatePassword); 
-
 //Importation des routes user et sauces
 // app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/api/sauces', sauceRoutes);
-
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
 
 module.exports = app;
